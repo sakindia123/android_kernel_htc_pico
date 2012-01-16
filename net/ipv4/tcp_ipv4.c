f983d7b54eeedc09f4d4b7b398166a39e125f8fe
franciscofranco <franciscofranco.1990@gmail.com>
2012-01-16 16:37:06 +0000

    neither TCP resets nor ICMP destination-unreachable packets will be sent in response to packets sent to ports for which no associated listening process exists

--- net/ipv4/tcp_ipv4.c
+++ net/ipv4/tcp_ipv4.c
@@ -1615,6 +1615,7 @@ int tcp_v4_do_rcv(struct sock *sk, struct sk_buff *skb)
 	return 0;
 
 reset:
+	if (!skb->dev || (skb->dev->flags & IFF_LOOPBACK))
 	tcp_v4_send_reset(rsk, skb);
 discard:
 	kfree_skb(skb);
@@ -1732,6 +1733,7 @@ no_tcp_socket:
 bad_packet:
 		TCP_INC_STATS_BH(net, TCP_MIB_INERRS);
 	} else {
+		if (skb->dev->flags & IFF_LOOPBACK)
 		tcp_v4_send_reset(NULL, skb);
 	}
 

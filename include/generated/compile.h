c5fb6c7417ae5f1333a3a916fd5eafc6e26cbbbb
sakindia123 <sa6915@gmail.com>
2013-01-03 18:47:01 +0530

    Derp

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 9 */
+/* This file is auto generated, version 10 */
 /*  PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#9 PREEMPT Thu Jan 3 18:21:11 IST 2013"
+#define UTS_VERSION "#10 PREEMPT Thu Jan 3 18:28:18 IST 2013"
 #define LINUX_COMPILE_BY "root"
 #define LINUX_COMPILE_HOST "sarthak"
 #define LINUX_COMPILER "gcc version 4.4.3 (GCC) "

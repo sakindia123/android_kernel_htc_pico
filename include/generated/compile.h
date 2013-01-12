9aa7de554f7541b0a0c916ab0ab406af432531f0
sakindia123 <sa6915@gmail.com>
2013-01-12 22:50:54 +0530

    make mrproper

--- include/generated/compile.h
+++
@@ -1,7 +0,0 @@
-/* This file is auto generated, version 68 */
-/*  PREEMPT */
-#define UTS_MACHINE "arm"
-#define UTS_VERSION "#68 PREEMPT Fri Jan 11 22:02:08 IST 2013"
-#define LINUX_COMPILE_BY "root"
-#define LINUX_COMPILE_HOST "sarthak"
-#define LINUX_COMPILER "gcc version 4.4.3 (GCC) "
--- include/generated/utsrelease.h
+++
@@ -1,1 +0,0 @@
-#define UTS_RELEASE "3.0.16-pico-kernel3-kangbang"

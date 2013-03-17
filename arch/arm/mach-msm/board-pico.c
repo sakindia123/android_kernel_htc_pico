55205c237337f895fa1ce435eab7bd121dcf9ae4
sakindia123 <sa6915@gmail.com>
2013-03-17 16:38:02 +0530

    board-pico:Rework memory

--- arch/arm/mach-msm/board-pico.c
+++ arch/arm/mach-msm/board-pico.c
@@ -210,8 +210,8 @@ static struct msm_i2c_platform_data msm_gsbi1_qup_i2c_pdata = {
 
 
 #ifdef CONFIG_ARCH_MSM7X27A
-#define MSM_PMEM_MDP_SIZE       0x1500000
-#define MSM_PMEM_ADSP_SIZE      0x1200000
+#define MSM_PMEM_MDP_SIZE       0x1400000  // 20M
+#define MSM_PMEM_ADSP_SIZE      0xC00000  // 12M
 #endif
 
 #ifdef CONFIG_USB_EHCI_MSM_72K

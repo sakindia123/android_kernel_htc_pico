e249fc0170df66616b6171688b0ba6cbb97da7fc
Nihar Gajare <nihargajre@gmail.com>
2013-06-09 10:55:27 +0530

    Bulk Update to 3.0.60

--- mm/compaction.c
+++ mm/compaction.c
@@ -732,7 +732,7 @@ int sysctl_compaction_handler(struct ctl_table *table, int write,
 			void __user *buffer, size_t *length, loff_t *ppos)
 {
 	if (write)
-		return compact_nodes();
+		compact_nodes();
 
 	return 0;
 }

d64a0f3e9627280492a41846d515e6d86a16b14e
sakindia123 <sa6915@gmail.com>
2013-08-09 16:19:19 +0530

    Use konstat's BT drivers

--- drivers/bluetooth/bluetooth-power.c
+++ drivers/bluetooth/bluetooth-power.c
@@ -21,12 +21,8 @@
 #include <linux/platform_device.h>
 #include <linux/rfkill.h>
 
-#if defined (CONFIG_MACH_LGE)
-#include <mach/board_lge.h>
-
-static struct bluetooth_platform_data *bt_platform_data = 0;
-#else /* origin */
 static bool previous;
+
 static int bluetooth_toggle_radio(void *data, bool blocked)
 {
 	int ret = 0;
@@ -40,26 +36,15 @@ static int bluetooth_toggle_radio(void *data, bool blocked)
 	return ret;
 }
 
-#endif
-
-/* LGE_CHANGES_S [taekeun1.kim@lge.com] 2010-06-06, for bt */
-#if defined (CONFIG_MACH_LGE)
-static struct rfkill_ops bluetooth_power_rfkill_ops;
-#else
 static const struct rfkill_ops bluetooth_power_rfkill_ops = {
 	.set_block = bluetooth_toggle_radio,
 };
-#endif
 
 static int bluetooth_power_rfkill_probe(struct platform_device *pdev)
 {
 	struct rfkill *rfkill;
 	int ret;
 
-/* LGE_CHANGES_S [taekeun1.kim@lge.com] 2010-06-06, for bt */
-#if defined (CONFIG_MACH_LGE)
-	bluetooth_power_rfkill_ops.set_block = bt_platform_data->bluetooth_toggle_radio;
-#endif
 	rfkill = rfkill_alloc("bt_power", &pdev->dev, RFKILL_TYPE_BLUETOOTH,
 			      &bluetooth_power_rfkill_ops,
 			      pdev->dev.platform_data);
@@ -71,9 +56,7 @@ static int bluetooth_power_rfkill_probe(struct platform_device *pdev)
 
 	/* force Bluetooth off during init to allow for user control */
 	rfkill_init_sw_state(rfkill, 1);
-#if !defined (CONFIG_MACH_LGE)
 	previous = 1;
-#endif
 
 	ret = rfkill_register(rfkill);
 	if (ret) {
@@ -111,9 +94,6 @@ static int __devinit bt_power_probe(struct platform_device *pdev)
 		return -ENOSYS;
 	}
 
-#if defined (CONFIG_MACH_LGE)
-	bt_platform_data = (struct bluetooth_platform_data *)pdev->dev.platform_data;
-#endif
 	ret = bluetooth_power_rfkill_probe(pdev);
 
 	return ret;

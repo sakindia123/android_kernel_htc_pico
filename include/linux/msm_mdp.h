179e21b57c85521eabcf88bb87bf46b47665fba0
Sachin Thomas <sachinthomaspj@gmail.com>
2014-06-23 18:36:27 +0530

    msm: display: Buffer sync point support
    Add MSMFB_BUFFER_SYNC ioctl for the communication of
    the acquired and released fence fd between kernel and hwc.
    
    Wait for acquired fence before pan display.
    Release the previous released fence after pan display
    
    Change-Id: I6534aed574a025d948581e70465f06a86b46c868
    Signed-off-by: Ken Zhang <kenz@codeaurora.org>

--- include/linux/msm_mdp.h
+++ include/linux/msm_mdp.h
@@ -69,6 +69,7 @@
 #define MSMFB_MDP_PP _IOWR(MSMFB_IOCTL_MAGIC, 156, struct msmfb_mdp_pp)
 #define MSMFB_OVERLAY_VSYNC_CTRL _IOW(MSMFB_IOCTL_MAGIC, 160, unsigned int)
 #define MSMFB_VSYNC_CTRL  _IOW(MSMFB_IOCTL_MAGIC, 161, unsigned int)
+#define MSMFB_BUFFER_SYNC  _IOW(MSMFB_IOCTL_MAGIC, 162, struct mdp_buf_sync)
 
 #define MSMFB_GET_USB_PROJECTOR_INFO _IOR(MSMFB_IOCTL_MAGIC, 301, struct msmfb_usb_projector_info)
 #define MSMFB_SET_USB_PROJECTOR_INFO _IOW(MSMFB_IOCTL_MAGIC, 302, struct msmfb_usb_projector_info)
@@ -464,6 +465,14 @@ struct msmfb_mdp_pp {
 	} data;
 };
 
+#define MDP_MAX_FENCE_FD	4
+
+struct mdp_buf_sync {
+	uint32_t flags;
+	uint32_t acq_fen_fd_cnt;
+	int *acq_fen_fd;
+	int *rel_fen_fd;
+};
 
 struct mdp_page_protection {
 	uint32_t page_protection;
